081f258377d8563e5482e38ab15f8d2b00215612
Aadeesh <aadeesh.2002@gmail.com>
2023-08-28 21:30:05 +0530

    Updated comments

--- Full_adder/FA_tb.v
+++ Full_adder/FA_tb.v
@@ -1,12 +1,12 @@
 `include "FA.v"
-module testbench;
+module FA_tb;
 
-    reg x,y,z;
-    wire s,c;
-    full_adder FA(.a(x), .b(y), .c1(z), .s(s), .c2(c));
+    reg x,y,z;  // a , b, c1
+    wire s,c;  // s, c2
+    full_adder FA(.a(x), .b(y), .c1(z), .s(s), .c2(c));  // instantiation of full adder module
     initial begin
         $dumpfile("fulladder.vcd");  // this file will be opened using gtkwave tool.
-        $dumpvars(0,testbench);
+        $dumpvars(0,FA_tb);
         $monitor($time, " x=%b, y=%b, z=%b, sum=%b, c=%b",x,y,z,s,c);
         x = 0;y = 0; z = 0;
         #5 x = 0;y = 1; z = 1;
