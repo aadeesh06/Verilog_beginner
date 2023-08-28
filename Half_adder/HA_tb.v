f0a8d79d678c6bf3e29f81cc7d47c41de3f4e3a5
Aadeesh <aadeesh.2002@gmail.com>
2023-08-28 21:34:24 +0530

    Updated path in line 1

--- Half_adder/HA_tb.v
+++ Half_adder/HA_tb.v
@@ -1,11 +1,11 @@
-`include "HA.v"
-module testbench;
+`include "Half_adder\HA.v"
+module HA_tb;
     reg x,y;
     wire s,c;
     Half_Adder DUT(.x(x), .y(y), .s(s), .c(c));
     initial begin
         $dumpfile("Half_Adder.vcd");
-        $dumpvars(0,testbench);
+        $dumpvars(0,HA_tb);
         $monitor($time, " x=%b, y = %b, s = %b, c = %b",x,y,s,c); 
         #5 x = 0; y = 0;
         #5 x = 0; y = 1;
